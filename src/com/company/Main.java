2ec022ee855e73a9ed899e1bb59eeb5f341ec8f2
topba <66887206+topba@users.noreply.github.com>
2021-06-17 17:42:55 +0300

    calculating the average of a a given array using the harmonic formula

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum +=1/ numbers[i];
+        }
+        System.out.println("the total :"+sum);
+    }
+}
